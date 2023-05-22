5513572b97dac567a78f5345b7fc151d4ed1adc3
Vaishali Jain <85574701+vaishalijain3456@users.noreply.github.com>
2023-05-22 11:22:29 +0530

    Create unique arithmetic triplets.cpp

---
+++ unique arithmetic triplets.cpp
@@ -0,0 +1,31 @@
+//You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. 
+//Return the number of unique arithmetic triplets.
+
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff)
+    {
+       
+      int n=nums.size();
+
+      int cnt=0; 
+
+
+      for(int i=0;i<n;i++)
+      {
+          for(int j=i+1;j<n;j++)
+          {
+              for(int k=j+1;k<n;k++)
+              {
+                  if(nums[k]-nums[j]==diff&&nums[j]-nums[i]==diff)
+                  {
+                      cnt++;
+                  }
+              }
+          }
+      }
+
+         return cnt;
+
+    }
+};
